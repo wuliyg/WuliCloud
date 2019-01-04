3e8bc4db10149fdc1c62bd61fa05ed943e4b4bd7
WuliBao <1435468902@qq.com>
2019-01-04 14:47:30 +0800

    WuliCloud 集成 Bus 和RabbitMq实现刷新获取配置文件

--- ConfigClientBus/src/main/java/com/wulis/config/client/bus/ConfigClientBusApplication.java
+++ ConfigClientBus/src/main/java/com/wulis/config/client/bus/ConfigClientBusApplication.java
@@ -1,25 +1,32 @@
-package com.wulis.config;
+package com.wulis.config.client.bus;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@EnableDiscoveryClient
 @EnableEurekaClient
 @SpringBootApplication
+@RefreshScope
 @RestController
-public class ConfigClientApplication {
+public class ConfigClientBusApplication {
 
     @Value("${config}")
     String config;
+
     @RequestMapping(value = "/getConfig")
-    public String getConfig(){
+    public String getConfig() {
         return config;
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigClientApplication.class, args);
+        SpringApplication.run(ConfigClientBusApplication.class, args);
     }
+
 }
+
